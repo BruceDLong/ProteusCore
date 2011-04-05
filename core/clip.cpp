62abde59d5cc6781579203a8eea0b426ef1f9419
Davide Del Vento <davide.del.vento@gmail.com>
2011-04-05 12:07:14 -0600

    preliminary support for readline

--- core/clip.cpp
+++ core/clip.cpp
@@ -8,6 +8,59 @@ std::fstream log;
 
 using namespace std;
 
+
+// maybe wrat this whole in a IFDEF clause?
+
+#include <readline/readline.h>
+#include <readline/history.h>
+
+/* A static variable for holding the line. */
+static char *line_read = (char *)NULL;
+
+/* Read a string, and return a pointer to it.
+   Returns "quit" on EOF. */
+std::string
+rl_gets (char* prompt)
+{
+  /* If the buffer has already been allocated,
+     return the memory to the free pool. */
+  if (line_read)
+    {
+      free (line_read);
+      line_read = (char*)NULL;
+    }
+
+  /* Get a line from the user. */
+  line_read = readline (prompt);
+
+  /* If the line has any text in it,
+     save it on the history. */
+  if (line_read && *line_read)
+    add_history (line_read);
+
+  if (line_read == NULL)
+  {
+    line_read = (char*)"quit";
+  }
+    std::string output(line_read);
+  return (output);
+}
+
+std::string HIST_FILE(getenv("HOME"));
+void rl_init() {
+  HIST_FILE.append("/.proteus.clip.hist");
+  int a = read_history (HIST_FILE.c_str());
+  stifle_history (1000);
+  std::cout << "History " << HIST_FILE  << " read with exit code " << a << std::endl;
+}
+
+void rl_finalize() {
+  int a = write_history (HIST_FILE.c_str());
+  std::cout << "History " << HIST_FILE << " written with exit code " << a << std::endl;
+}
+
+// up-to-here maybe wrat this whole in a IFDEF clause?
+
 char getCH(){
     char ch;
     if (cin.eof()||cin.fail()) {cout << "END OF FILE\n"; exit(1);}
@@ -15,9 +68,13 @@ char getCH(){
     return ch;
 }
 
-void readln(std::string &str){
-    char ch; str="";
-    for(ch=getCH(); ch!='\n';ch=getCH())str+=ch;
+std::string readln(std::string prompt){
+    // char ch; 
+    std::string str="";
+    // cout << prompt;
+    //for(ch=getCH(); ch!='\n';ch=getCH())str+=ch;
+    str = rl_gets(prompt.c_str());
+    return str;
 }
 #include <signal.h>
 
@@ -26,13 +83,13 @@ extern infon *World;
 infon* topInfon;
 
 int main(int argc, char **argv){
+        rl_init(); // with IFDEF readline?
 	signal(SIGSEGV, reportFault);
 	topInfon=World;  // use topInfon in the ddd debugger to view World
 	cout<<"\nThe Proteus CLI. Type some infons or 'quit'\n\n";
 	agent a;
 	while(!cin.eof()){
-		cout << "Proteus: ";
-		string entry=""; readln(entry);
+		std::string entry= readln("Proteus: ");
 		if (entry=="quit") break;
 		if (entry=="") continue;
 		//char ch='x', pr; do {pr=ch; ch=getCH(); entry+=ch;} while (!(pr=='%' && ch=='>'));
@@ -47,7 +104,7 @@ int main(int argc, char **argv){
 		if (Entry) cout<<"\n"<<printInfon(Entry)<<"\n\n";
 		else {cout<<"\nError: "<<q.buf<<"\n\n";}
 	}
-
+        rl_finalize(); // with IFDEF readline?
  return 0;
 }
 
